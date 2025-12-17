089ee84dbab890b861e478e91799762598cf83a2
aprianfirlanda <aprianfirlanda@gmail.com>
2025-12-18 06:20:02 +0700

    refactor(db): update pinger implementation to use a factory method

--- cmd/http.go
+++ cmd/http.go
@@ -23,7 +23,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		dbPinger := gorm.Pinger{DB: dbConn}
+		dbPinger := gorm.NewPinger(dbConn)
 
 		return http.StartServer(bootstrap.HttpApp{
 			Config:   cfg,
--- internal/adapter/outbound/persistance/gorm/pinger.go
+++ internal/adapter/outbound/persistance/gorm/pinger.go
@@ -3,12 +3,17 @@ package gorm
 import (
 	"context"
 	"go-archetype/internal/infrastructure/database"
+	"go-archetype/internal/ports/outbound"
 
 	"gorm.io/gorm"
 )
 
 type Pinger struct {
 	DB *gorm.DB
+}
+
+func NewPinger(db *gorm.DB) outbound.DBPinger {
+	return &Pinger{DB: db}
 }
 
 func (p Pinger) Ping(ctx context.Context) error {
